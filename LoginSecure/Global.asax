52cea18daf5ae0517620a10e4ea3028e150322a6
ADMIN\Administrator <dinhquoc04b@gmail.com>
2018-10-05 13:53:43 +0700

    update 1

---
+++ LoginSecure/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginSecure.MvcApplication" Language="C#" %>
---
+++ LoginSecure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoginSecure.Startup))]
+namespace LoginSecure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
